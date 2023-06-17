7fbbedbdb86ca222950b8889871eb02f0a7464a8
André Devincenzi <andre.devincenzi@hotmail.com>
2023-06-17 00:24:21 -0300

    alteração no template de contato

--- resources/views/emails/contato.blade.php
+++ resources/views/emails/contato.blade.php
@@ -1,6 +1,29 @@
-<div class='w-full bg-slate-900 text-center'>
-    <h1 class='text-slate-100 border-b border-slate-600'>{{ $nomeContato }} enviou uma mensagem!</h1>
-    <p class='w-full text-justify text-slate-100'>
+<div class='container'>
+    <h2 class='titulo'>{{ $nomeContato }} enviou uma mensagem!</h2>
+    <p class='mensagem'>
         {{ $mensagem }}
     </p>
 </div>
+
+<style>
+    .container{
+        width: 100%;
+        background-color: #0f172a;
+        display: flex;
+        justify-content: center;
+        border: 1px #475569 solid;
+    }
+
+    .titulo{
+        color: #f1f5f9;
+        border: 1px #475569 solid;
+    }
+
+    .mensagem{
+        color: #f1f5f9;
+    }
+
+    .mensagem::first-letter{
+        text-transform: uppercase;
+    }
+</style>
