3181212058c5c89ed5d09db46e2f2882c35455c9
André Devincenzi <andre.devincenzi@hotmail.com>
2023-06-17 01:14:12 -0300

    alteração no template de contato

--- resources/views/emails/contato.blade.php
+++ resources/views/emails/contato.blade.php
@@ -9,18 +9,24 @@
     .container{
         width: 100%;
         background-color: #0f172a;
-        border: 1px #475569 solid;
+        border: 2px #475569 solid;
+        border-radius: 10px;
     }
 
     .titulo{
         color: #f1f5f9;
         width: 100%;
         text-align: center;
-        border: 1px #475569 solid;
+        border-bottom: 1px #475569 solid;
+        padding: 1rem 0;
     }
 
     .mensagem{
         color: #f1f5f9;
+        font-size: large;
+        padding: 1rem;
+        line-height: 1.25rem;
+        text-align: justify;
     }
 
     .mensagem::first-letter{
